772abe4d39fd0b5c653755061c86476599e0eb6c
Jakub Piasecki <jakub@piaseccy.pl>
2019-03-10 16:14:54 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ SSH_PATH="$HOME/.ssh"
 mkdir "$SSH_PATH"
 echo "$PRIVATE_DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
-cat "$SSH_PATH/deploy_key"
+
 # Do deployment
 ssh -i $SSH_PATH/deploy_key -o 'StrictHostKeyChecking=no' $USER@$HOST "cd $WORKSPACE && $*"
